384c5cb1c9a6734d2598db4017a7bc79ab8ca375
abhiishekpanchal <abhishekp.280403@gmail.com>
2025-06-15 23:02:56 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/drones', droneRoutes);
 app.use('/api/missions', missionRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB')
   })
